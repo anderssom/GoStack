6b2314259a8b4bd010fd6b0170a48fe0c3476b02
anderson <anderssom05@gmail.com>
2021-04-17 09:15:39 -0300

    versaõ2

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import {Router} from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../sevices/CreateUserService';
 
+import ensureAutheticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
+
 
 usersRouter.post('/', async (request, response) =>{
     try{
@@ -30,5 +36,14 @@ usersRouter.post('/', async (request, response) =>{
             return response.status(400).json({ error: err.message });
             }
 });
+
+usersRouter.patch(
+    '/avatar',
+     ensureAutheticated,
+     upload.single('avatar'),
+      async (request, response) => {
+        return response.json({ ok: true});
+      },
+);
 
 export default usersRouter;
